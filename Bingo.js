151c866af203f09cf7a02140bd49afe486bab918
Nax2l <aboyass9510a@gmail.com>
2026-03-14 04:31:58 +0300

    Update Bingo.js

--- Bingo.js
+++ Bingo.js
@@ -9,6 +9,16 @@ const {
 } = require('discord.js');
 const mongoose = require('mongoose');
 const { createCanvas, loadImage } = require('canvas');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
 
 // إعداد الكلاينت
 const client = new Client({
